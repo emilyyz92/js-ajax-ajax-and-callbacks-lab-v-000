add0650a914a59968e0a73eac86e6839652e14bd
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-12 21:43:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  let searchTerm = $('#searchTerms').val()
+  $.get(
+    `https://api.github.com/search/repositories?q=${searchTerm}`,
+    function(response) {
+      let responseList = '<ul'> + response.items.map(item =>
+        '<li>' + item.name + '</li>').join() + '</ul>'
+      $('#results').html(responseList)
+      debugger;
+  }).fail(displayError())
+}
+
+function displayError() {
+  let error = `Error, cannot find page`
+  $('#errors').html(error)
+}
