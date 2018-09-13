c26bc324430ca49d13d8df5fb0416ba8863baf3c
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-13 14:18:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ function searchRepositories() {
   const searchTerm = $('#searchTerms').val()
   $.get(
     `https://api.github.com/search/repositories?q=${searchTerm}`,
-    function(response) {
-      let responseList = '<ul'> + response.items.map(item =>
-        '<li>' + item.name +
-        '- <a onclick="showCommits" src="#">Show Commits</a>'
-        + '</li>').join() + '</ul>'
-      $('#results').html(responseList)
-
-  }).fail(
-    debugger;
+    response => displaySearch(response)).fail(
     displayError())
 }
 
