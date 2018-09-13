75d2e7005d460da4ac2d223755bc3a3be3dfb55b
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-13 14:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ function displayError() {
   let error = `Error, cannot find page`
   $('#errors').html(error)
 }
+
+function displaySearch(response) {
+  responseList = '<ul'> + response.items.map(item =>
+    '<li>' + item.name +
+    '- <a onclick="showCommits" src="#">Show Commits</a>'
+    + '</li>').join() + '</ul>'
+  $('#results').html(responseList)
+}
