cd897c873b217ed46871f41668381a41c8522571
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-13 15:03:48 +0000

    Done.

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ $(document).ready(function (){
 });
 
 function searchRepositories() {
-  let searchTerm = $('#searchTerms').val()
+  const searchTerm = $('#searchTerms').val()
+  debugger;
   $.get(
     `https://api.github.com/search/repositories?q=${searchTerm}`,
-    function(response) {
-      let responseList = '<ul'> + response.items.map(item =>
-        '<li>' + item.name + '</li>').join() + '</ul>'
-      $('#results').html(responseList)
-      debugger;
-  }).fail(displayError())
+    response => displaySearch(response)).fail(
+    displayError())
 }
 
 function displayError() {
   let error = `Error, cannot find page`
   $('#errors').html(error)
+}
+
+function displaySearch(response) {
+  responseList = '<ul'> + response.items.map(item =>
+    '<li>' + item.name +
+    '- <a onclick="showCommits" src="#">Show Commits</a>'
+    + '</li>').join() + '</ul>'
+  $('#results').html(responseList)
+}
+
+function showCommits() {
+
 }
